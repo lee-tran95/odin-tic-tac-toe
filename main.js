5d8f8e211a2bb5d88ed9141e27141cbec99d8f0b
Lee Tran <lee.tran95@yahoo.com>
2023-06-29 23:10:10 -0700

    Add win conditions to the game and displays message if there is a winner

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ startButton.addEventListener('click', () =>{
 
 const Gameboard = (() =>{
     const gameBoard = 
-        ['X',' ',' ',
-         ' ',' ',' ',
-         ' ',' ',' ']
+        ['','','',
+         '','','',
+         '','','']
 
     const displayBoard = () =>{
         Array.from(document.querySelectorAll(".grid"), (grid,index) =>{
@@ -32,8 +32,9 @@ const DisplayController = (() =>{
     let gameOver;
 
     const start = () =>{
-        let player1 = PlayerFactory(document.getElementById('player1').value, 'X')
-        let player2 = PlayerFactory(document.getElementById('player2').value, 'O')
+        if(players.length > 0) return
+        let player1 = PlayerFactory(document.getElementById('player1').value || "1", 'X')
+        let player2 = PlayerFactory(document.getElementById('player2').value || "2", 'O')
         players.push(player1, player2)
         currentPlayer = 0;
         gameOver = false
@@ -41,15 +42,40 @@ const DisplayController = (() =>{
     }
 
     const mark = (event) => {
-        if(event.target.textContent === 'X' || event.target.textContent === 'O'){
+        if(event.target.textContent === 'X' || event.target.textContent === 'O' || gameOver){
             return
         }
         Gameboard.gameBoard[event.target.dataset.index] = players[currentPlayer].marker
-        currentPlayer = currentPlayer === 0 ? 1 : 0
         Gameboard.displayBoard()
+        if(winConditions()){
+            document.getElementById('winner-message').textContent = `Player ${players[currentPlayer].name} Wins!`
+            gameOver = true
+        }
+        currentPlayer = currentPlayer === 0 ? 1 : 0
+    }
+
+    const winConditions = () =>{
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+        for(let i = 0; i < winningCombinations.length; i++){
+            const [a, b, c] = winningCombinations[i]
+            if(Gameboard.gameBoard[a] && Gameboard.gameBoard[a] === Gameboard.gameBoard[b] && Gameboard.gameBoard[a] === Gameboard.gameBoard[c]){
+                return true
+            }
+        }
+        return false
     }
     return{
         start,
-        mark
+        mark,
+        winConditions
     }
 })();
